df6cb408469b65a8afe0300c36eb1c89debe04f2
Curran McConnell <curran@atidot.com>
2020-02-10 17:18:07 -0500

    Improved vector operations on Spectrum objects

--- spectrum.py
+++ spectrum.py
@@ -1,11 +1,9 @@
+import numpy as np
 from numpy import fft
 import math
 
 class Spectrum(list):
-    LENGTH_OF_PLAY = 2.0 # in seconds
-    HERZ_INTERVAL = 0.5
-    MAX_HERZ = 44100
-    BLUEPRINT = [0.0] * math.floor((1 + (LENGTH_OF_PLAY * MAX_HERZ / HERZ_INTERVAL)))
+    BLUEPRINT = [0.0] * 44100
 
     def __init__(self):
         super().__init__(self.BLUEPRINT)
@@ -37,19 +35,42 @@ class Spectrum(list):
         for (freq, amp) in {**unique_dict, **shared_dict}.items():
             output[freq] = amp
         return output
-
-    def __mul__(self, scalar : float):
-        if type(scalar) != float:
-            raise TypeError
-        new_freqs = {freq: self[freq] * val for freq in self.nonzero_freqs}
-        output = Spectrum()
-        for (freq, val) in new_freqs.items():
-            output[freq] = val
-        return output
+    
+    def __mul__(self, other):
+        if type(other) == float:
+            scalar = other
+            new_freqs = {freq: self[freq] * val for freq in self.nonzero_freqs}
+            output = Spectrum()
+            for (freq, val) in new_freqs.items():
+                output[freq] = val
+            return output
+        elif isinstance(other, Spectrum):
+            if len(self.nonzero_freqs) > len(other.nonzero_freqs):
+                s1 = other
+                s2 = self
+            else:
+                s1 = self
+                s2 = other
+            return sum([s1[freq] * s2[freq] for freq in s1.nonzero_freqs])
 
     def as_wave(self):
-        formattedForNumpy = [0.0, *self]
+        formattedForNumpy = np.asarray([0.0, *self], dtype=np.int16)
         return list(map(lambda x: x.real, fft.ifft(formattedForNumpy)))
+
+    def __abs__(self, p):
+        if p == float('inf'):
+            return max(list(map(abs, self)))
+
+        if p < 1:
+            raise ValueError("The p-norm exists for p in [1, inf].")
+        
+        accum = 0.0
+        for elt in self:
+            accum += abs(elt)**p
+        return accum**(1/p)
+
+    def __abs__(self):
+        return abs(self, 2)
 
 # A Voice is a spectrum relative to a fundamental, which must remain at amplitude 1.0.
 class Voice(Spectrum):
